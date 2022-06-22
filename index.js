b6d58eaa5fe593922d57a5751fefc63beb7146d6
dudeVladimir <dude.vladimit@mail.ru>
2022-06-22 15:36:17 +0300

    checked

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ import faculties from './faculties.js'
 const checked = []
 
 function renderRows(arr, $el) {
-  if (arr.length === 0) {
+  $el.querySelectorAll('button').forEach((e) => {
+    arr.length === 0 ? (e.disabled = true) : (e.disabled = false)
+  })
+
+  if (arr.length === 0 && !$el.querySelector('.empty')) {
     const $tr = document.createElement('tr')
     $tr.classList.add('empty')
     $tr.innerHTML = `
@@ -14,16 +18,11 @@ function renderRows(arr, $el) {
     <td></td>
   `
     $el.append($tr)
-    $el.querySelectorAll('button').forEach((e) => {
-      e.classList.add('disabled')
-    })
+    if ($el.querySelector('.body')) $el.querySelector('.body').remove()
   } else {
-    if ($el.querySelector('.body')) {
-      $el.querySelector('.body').remove()
-    }
-    if ($el.querySelector('.empty')) {
-      $el.querySelector('.empty').remove()
-    }
+    if ($el.querySelector('.body')) $el.querySelector('.body').remove()
+    if ($el.querySelector('.empty')) $el.querySelector('.empty').remove()
+
     const $tbody = document.createElement('tbody')
     $tbody.classList.add('body')
     $el.append($tbody)
@@ -32,38 +31,64 @@ function renderRows(arr, $el) {
       const $tr = document.createElement('tr')
 
       $tr.innerHTML = `
-      <td><input type="checkbox" id=${e.data.name}-${e.id}></td>
+      <td><input type="checkbox" id=${e.id}></td>
       <td>${e.id}</td>
       <td>${e.data.name}</td>
     `
       $tbody.append($tr)
-      $el.querySelectorAll('button').forEach((e) => {
-        e.classList.remove('disabled')
-      })
     })
+    addEl($el)
   }
 }
-renderRows(faculties, $facultiesTable)
-renderRows(checked, $checkedTable)
 
-$facultiesTable.querySelectorAll('button').forEach((e) => {
-  if (e.textContent === 'ID') {
-    e.addEventListener('click', () => {
-      renderRows(
-        faculties.sort((a, b) => {
-          if (a.id < b.id) return -1
-          if (a.id > b.id) return 1
-          return 0
-        }),
-        $facultiesTable
-      )
-    })
-  } else {
-    e.addEventListener('click', () => {
-      renderRows(
-        faculties.sort((a, b) => a.data.name.localeCompare(b.data.name)),
-        $facultiesTable
-      )
+sortArr(checked, $checkedTable)
+sortArr(faculties, $facultiesTable)
+
+function sortArr(arr, $el) {
+  $el.querySelectorAll('button').forEach((e) => {
+    if (e.textContent === 'ID') {
+      e.addEventListener('click', () => {
+        renderRows(
+          arr.sort((a, b) => {
+            if (a.id < b.id) return -1
+            if (a.id > b.id) return 1
+            return 0
+          }),
+          $el
+        )
+      })
+    } else {
+      e.addEventListener('click', () => {
+        renderRows(
+          arr.sort((a, b) => a.data.name.localeCompare(b.data.name)),
+          $el
+        )
+      })
+    }
+  })
+}
+
+function addEl($el) {
+  const $inputs = $el.querySelectorAll('input')
+  if ($el === $checkedTable) {
+    $inputs.forEach((e) => {
+      e.checked = true
     })
   }
-})
+  $inputs.forEach((e, idx) => {
+    e.addEventListener('click', () => {
+      if ($el === $facultiesTable) {
+        checked.push(faculties[idx])
+        faculties.splice(idx, 1)
+      } else {
+        faculties.push(checked[idx])
+        checked.splice(idx, 1)
+      }
+      renderRows(faculties, $facultiesTable)
+      renderRows(checked, $checkedTable)
+    })
+  })
+}
+
+renderRows(faculties, $facultiesTable)
+renderRows(checked, $checkedTable)
